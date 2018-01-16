b82b0d534bf25af407b54bd15c430ff49d65cb80
adamhirata <35279376+adamhirata@users.noreply.github.com>
2018-01-16 13:07:03 -0800

    Add files via upload
    I think I fixed the empty sub-directory problem.

---
+++ homework00/Hello.java
@@ -0,0 +1,11 @@
+/*==================================
+  Adam Hirata
+  1/15/18
+  Homework00
+==================================*/
+
+public class Hello {
+    public static void main( String args[] ) {
+        System.out.println( "\nHello World!" );
+    }
+}
